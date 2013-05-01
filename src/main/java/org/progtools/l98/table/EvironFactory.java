e6c8ff4635008195a19df7eaa64a9e47045cd911
Paulo Pinto <pjmlp@progtools.org>
2013-05-01 11:13:48 +0200

    Adding a new class to set up the initial environment.

---
+++ src/main/java/org/progtools/l98/table/EvironFactory.java
@@ -0,0 +1,82 @@
+/* EvironFactory.java : Creates an pre-configured environment for L98.
+ * Copyright (C) 2013  Paulo Pinto
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+package org.progtools.l98.table;
+
+import org.progtools.l98.Attributes;
+import org.progtools.l98.access.Access;
+import org.progtools.l98.access.Builtin;
+import org.progtools.l98.type.Type;
+import org.progtools.l98.type.TypeArg;
+import org.progtools.l98.type.TypeBool;
+import org.progtools.l98.type.TypeFunc;
+import org.progtools.l98.type.TypeInt;
+import org.progtools.l98.type.TypeProc;
+import org.progtools.l98.util.List;
+
+/**
+ * Creates an pre-configured environment for L98.
+ */
+public final class EvironFactory {
+    public static Environ getEnvironment() {
+        Environ env = new Environ (); 
+        List args;
+        Type subType;
+        Attributes attr;
+        Access subAccess;
+
+        // Creates the readint function
+        subType = new TypeFunc (new List (), new TypeInt (), 1);
+        subAccess = new Builtin (Builtin.READINT);
+        attr = new Attributes (subType, subAccess, false, false);
+        env.update ("readint", attr);
+
+
+        // Creates the readbool function
+        subType = new TypeFunc (new List (), new TypeBool (), 1);
+        subAccess = new Builtin (Builtin.READBOOL);
+        attr = new Attributes (subType, subAccess, false, false);
+        env.update ("readbool", attr);
+
+        // Creates the printint procedure
+        args = new List ();
+        args.pushBack (new TypeArg (false, new TypeInt ()));
+
+        subType = new TypeProc (args);
+        subAccess = new Builtin (Builtin.PRINTINT);
+        attr = new Attributes (subType, subAccess, false, false);
+        env.update ("printint", attr);
+
+        // Creates the printbool procedure
+        args = new List ();
+        args.pushBack (new TypeArg (false, new TypeBool ()));
+
+        subType = new TypeProc (args);
+        subAccess = new Builtin (Builtin.PRINTBOOL);
+        attr = new Attributes (subType, subAccess, false, false);
+        env.update ("printbool", attr);
+
+        // Creates the println procedure
+        subType = new TypeProc (new List ());
+        subAccess = new Builtin (Builtin.PRINTLN);
+        attr = new Attributes (subType, subAccess, false, false);
+        env.update ("println", attr);
+
+        return env;
+    }
+}
