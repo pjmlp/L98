6577461864a84d4602bdbc261ef2ec85b81b3e5d
Paulo Pinto <pjmlp@progtools.org>
2013-05-01 21:10:00 +0200

    Utility class for filename replacements.

---
+++ src/main/java/org/progtools/l98/util/Pathnames.java
@@ -0,0 +1,36 @@
+/* Pathnames.java : Utility class for pathname handling.
+ * Copyright (C) 1999  Paulo Pinto, Pablo Tavares
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+package org.progtools.l98.util;
+
+import java.io.File;
+
+/**
+ * Utility class for pathname handling.
+ */
+public final class Pathnames {
+    /*
+     * Changes the extension of the provided path.
+     * @return the same pathname with the extension replaced
+     */
+    public static String changeFileExtension(String filename, String extension) {
+        return filename.substring(filename.lastIndexOf(File.separatorChar) + 1, filename.lastIndexOf('.')) + extension;
+    }
+    
+    
+}
