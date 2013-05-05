bfb1ccc43689570e5d7760b5e15c126265446bfe
Paulo Pinto <pjmlp@progtools.org>
2013-05-05 13:35:08 +0200

    Cleaned up comments.

--- l98c.cmd
+++ l98c.cmd
@@ -16,6 +16,7 @@
 # License along with this library; if not, write to the
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
+#java -jar L98-0.0.1-SNAPSHOT.jar $1 $2
 
 JVM=`which java`
 if [ ! -x $JVM ]; then
